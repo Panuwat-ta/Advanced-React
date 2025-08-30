04811467083c1e7145012494cb1114e0adbf2ca1
panuwat-ta <panuwattakham2002@gmail.com>
2025-08-31 05:14:40 +0700

    TODO 3 และ TODO 4

--- mini-ecommerce/src/features/cart/cartSlice.js
+++ mini-ecommerce/src/features/cart/cartSlice.js
@@ -18,7 +18,18 @@ const cartSlice = createSlice({
       }
     },
     // TODO 3: removeItem
+    removeItem(state, action) {
+      const idToRemove = action.payload; // payload = product id
+      state.items = state.items.filter(item => item.id !== idToRemove);
+    },
     // TODO 4: updateQuantity
+    updateQuantity(state, action) {
+      const { id, quantity } = action.payload;
+      const item = state.items.find(item => item.id === id);
+      if (item) {
+        item.quantity = quantity;
+      }
+    },
   },
 });
 
